4e621cd3a67b9423baf2406f1b67bbf1fc474dc1
roma patel <romapatel614@gmail.com>
2022-05-23 00:13:37 -0400

    added shopping-list component

---
+++ shopping-list/src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+//This is one method you can define variable and constructor
+// export class Ingredient {
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount:number){
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
+export class Ingredient{
+    constructor(public name: string, public amount: number) {
+        
+    }
+}
