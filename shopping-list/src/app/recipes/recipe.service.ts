99fadbb49c3719d66634a504b3eccb905ea4100b
roma patel <romapatel614@gmail.com>
2022-05-23 21:19:49 -0400

    implementing shopping-list service

--- shopping-list/src/app/recipes/recipe.service.ts
+++ shopping-list/src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -6,15 +8,38 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
-  
-  private recipes:  Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', '../../assets/images/samosa_recipe.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', '../../assets/images/samosa_recipe.jpg')
+
+  private recipes: Recipe[] = [
+    new Recipe('Samosa',
+      'Cripsy hot pockets filled with tangy potato stuffing with Coriander chutney!',
+      '../../assets/images/samosa_recipe.jpg',
+      [
+        new Ingredient("Potato", 5),
+        new Ingredient("Green Peas", 50),
+        new Ingredient("Green Chilli", 4),
+        new Ingredient("Garlic", 3),
+        new Ingredient("Coriander", 5)
+      ]),
+    new Recipe(
+      'Pavbhaji',
+      'Mix veg curry with bun and onions on side!',
+      '../../assets/images/pavbhaji.jpg',
+      [
+        new Ingredient("Potato", 5),
+        new Ingredient("Green Peas", 50),
+        new Ingredient("Onion", 4),
+        new Ingredient("Tomato", 3),
+        new Ingredient("Coriander", 5)
+      ])
   ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
